28fe62e869b524b349ceba855d0e725fdd1975d5
Gregoire Lamy <g.aderlamy@gmail.com>
2020-01-25 17:36:06 +0100

    done with very few css

---
+++ starter-code/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> Say Hello to React JS man ! </h1>
+        <h2> You will learn a FrontEnd Frame wooork ! </h2>
+        <button className="bouton">Awesome !</button>
+      </div>
+    );
+  }
+}
+
+export default App;
